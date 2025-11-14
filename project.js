4adbdd49d2a31b2d28be3133a95c2bf1d7283e16
Gibum(David) Kim <gka77@sfu.ca>
2025-11-13 17:56:33 -0800

    Functional gallery layout into project details

---
+++ project.js
@@ -0,0 +1,60 @@
+// projects.js
+const projects = {
+    "samwoo-median": {
+        title: "삼우메디안 예산공장 증축공사 2016",
+        desc: "위치: 충청남도 예산군 신암면추사로 235-15, 대지면적: 8,122㎡, 규모: 지상 3층",
+        images: [
+            "projects/projectimages/삼우메디안1.png",
+            "projects/projectimages/삼우메디안2.png"
+        ]
+    },
+
+    "bio-cnd": {
+        title: "바이오씨앤디 강릉공장 증축공사 2016",
+        desc: "위치: 강원도 강릉시 대전동 916-5, 대지면적: 10,531㎡, 규모: 지상 3층",
+        images: [
+            "projects/projectimages/바이오씨앤디1.png",
+            "projects/projectimages/바이오씨앤디2.png"
+        ]
+    },
+
+    "hana-gmp": {
+        title: "하나제약(주) 발안 GMP공장",
+        desc: "위치: 경기도 화성시 향남면 발안지방산업단지 D30-1, 연면적: 4,716㎡, 규모: 지상 3층",
+        images: [
+            "projects/projectimages/하나제약1.png"
+        ]
+    },
+
+    "hugelkorea": {
+        title: "휴젤(주) 춘천공장 신축공사",
+        desc: "위치: 강원도 춘천시 동내면 거두리 1180, 연면적: 7,668㎡, 규모: 지상 3층 / 지하 1층",
+        images: [
+            "projects/projectimages/휴젤1.png"
+        ]
+    },
+
+    "hanmi": {
+        title: "한미약품(주) 평택공장 신축공사",
+        desc: "위치: 경기도 평택시 팽성읍 추팔리 395, 연면적: 19,267㎡, 규모: 지상 3층 / 지하 1층",
+        images: [
+            "projects/projectimages/한미약품1.png"
+        ]
+    },
+
+    "koreacorus": {
+        title: "한국코러스 제약(주) 춘천공장 증축공사",
+        desc: "위치: 강원도 춘천시 동내면 거두리 1181번지, 연면적: 2,388㎡, 규모: 지상 3층",
+        images: [
+            "projects/projectimages/한국코러스1.png"
+        ]
+    },
+
+    "stemcell": {
+        title: "국립 줄기세포·재생센터 건립공사",
+        desc: "위치: 충청북도 청원군 오승읍 연제리 663-2, 연면적: 6,668㎡, 규모: 지상 5층 / 지하 1층",
+        images: [
+            "projects/projectimages/줄기세포·재생센터1.png"
+        ]
+    }
+};
