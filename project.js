83f0b15685a8a812b2d82a772971e81122ae4abd
Gibum(David) Kim <gka77@sfu.ca>
2025-11-14 16:39:19 -0800

    All other project pages updated to have the gallery format

--- project.js
+++ project.js
@@ -82,6 +82,576 @@ const projects = {
         images: [
             "projects/projectimages/오성산1.png"
         ]
+    },
+
+    "cultural-arum": {
+    title: "성북동 아름갤러리 2016",
+    desc: "위치: 서울시 성북구 성북동, 연면적: 234.18 ㎡ / 288.57 ㎡, 대지면적: 271 ㎡ / 145 ㎡, 용도: 제 2종 근린생활시설 / 문화 및 집회시설",
+    images: [
+        "projects/projectimages/아름갤러리1.png",
+        "projects/projectimages/아름갤러리2.png",
+        "projects/projectimages/아름갤러리3.png"
+        ]
+    },
+
+    "cultural-deokcheon": {
+        title: "안양 덕천지구 2016",
+        desc: "위치: 경기도 안양시 덕천지구, 연면적: 1,363.92 ㎡, 대지면적: 500 ㎡, 용도: 교육연구시설 / 제 1종 근린생활시설",
+        images: [
+            "projects/projectimages/덕천지구1.png",
+            "projects/projectimages/덕천지구2.png",
+            "projects/projectimages/덕천지구3.png"
+        ]
+    },
+
+    "cultural-public": {
+        title: "부천시 복합공공시설 연구용역 2012",
+        desc: "위치: 경기도 부천시 소사구 괴안동, 연면적: 8,838.08 ㎡, 규모: 지상 6층",
+        images: [
+            "projects/projectimages/복합공공시설1.png",
+            "projects/projectimages/복합공공시설2.png",
+            "projects/projectimages/복합공공시설3.png",
+            "projects/projectimages/복합공공시설4.png"
+        ]
+    },
+
+    "cultural-ace": {
+        title: "서초동 에이스빌딩 리모델링공사 2015",
+        desc: "위치: 서울시 서초구 서초동 1720-2번지, 연면적: 7,805.04 ㎡, 규모: 지상 5층 / 지하 4층",
+        images: [
+            "projects/projectimages/에이스빌딩1.png"
+        ]
+    },
+
+    "commercial-gumi-fund": {
+    title: "구미 환경개선펀드 2016",
+    desc: "위치: 경상북도 구미시 공단동, 연면적: 43,582.80㎡, 대지면적: 8,132㎡, 용도: 공동주택 / 업무시설 / 제2종 근린생활시설",
+    images: [
+        "projects/projectimages/구미환경개선펀드1.png",
+        "projects/projectimages/구미환경개선펀드2.png",
+        "projects/projectimages/구미환경개선펀드3.png"
+    ]
+    },
+
+    "commercial-gumi-industrial": {
+        title: "구미 지식산업센터 2015",
+        desc: "위치: 경상북도 구미시 공단동, 연면적: 24,727.70㎡, 대지면적: 8,132㎡, 용도: 지식산업센터 / 지원시설",
+        images: [
+            "projects/projectimages/구미지식산업센터1.png",
+            "projects/projectimages/구미지식산업센터2.png"
+        ]
+    },
+
+    "commercial-wellstone": {
+        title: "웰스톤시티 입면 및 조경차별화 2014",
+        desc: "위치: 용인시 수지구 신봉동, 연면적: 21,839.07㎡, 대지면적: 6,606㎡, 용도: 근린생활시설 / 공동주택",
+        images: [
+            "projects/projectimages/웰스톤시티1.png",
+            "projects/projectimages/웰스톤시티2.png",
+            "projects/projectimages/웰스톤시티3.png",
+            "projects/projectimages/웰스톤시티4.png",
+            "projects/projectimages/웰스톤시티5.png"
+        ]
+    },
+
+    "commercial-koreaceramic": {
+        title: "한국도자기 사옥 리모델링 2014",
+        desc: "위치: 서울시 강남구 논현동, 연면적: 1,383.08㎡, 대지면적: 444.3㎡, 용도: 업무시설 / 근린생활시설",
+        images: [
+            "projects/projectimages/한국도자기1.png",
+            "projects/projectimages/한국도자기2.png",
+            "projects/projectimages/한국도자기3.png",
+            "projects/projectimages/한국도자기4.png"
+        ]
+    },
+
+    "commercial-sehwa": {
+        title: "세화사옥 신축공사 2014",
+        desc: "위치: 서울시 강서구 방화동, 연면적: 2,894.66㎡, 대지면적: 573.48㎡, 용도: 근린생활시설",
+        images: [
+            "projects/projectimages/세화사옥1.png",
+            "projects/projectimages/세화사옥2.png",
+            "projects/projectimages/세화사옥3.png"
+        ]
+    },
+
+    "commercial-gwanghui": {
+        title: "광희동 근린생활시설 2013",
+        desc: "위치: 서울시 중구 광희동, 연면적: 601.48㎡, 대지면적: 234.33㎡, 용도: 근린생활시설",
+        images: [
+            "projects/projectimages/광희동1.png",
+            "projects/projectimages/광희동2.png",
+            "projects/projectimages/광희동3.png",
+            "projects/projectimages/광희동4.png"
+        ]
+    },
+
+    "commercial-sinnae": {
+        title: "신내동 근린생활시설 2013",
+        desc: "위치: 서울시 중랑구 신내동, 연면적: 987.56㎡, 대지면적: 378㎡, 용도: 근린생활시설",
+        images: [
+            "projects/projectimages/신내동1.png",
+            "projects/projectimages/신내동2.png",
+            "projects/projectimages/신내동3.png",
+            "projects/projectimages/신내동4.png"
+        ]
+    },
+
+    "commercial-anyang-hospital": {
+        title: "안양 우리병원 2013",
+        desc: "위치: 경기도 안양시 동안구 비산동, 연면적: 4,925.23㎡, 대지면적: 1,361㎡, 용도: 의료시설",
+        images: [
+            "projects/projectimages/안양우리병원1.png",
+            "projects/projectimages/안양우리병원2.png",
+            "projects/projectimages/안양우리병원3.png",
+            "projects/projectimages/안양우리병원4.png"
+        ]
+    },
+
+    "commercial-samsung-office": {
+        title: "삼성동 업무시설 2013",
+        desc: "위치: 서울시 강남구 삼성동, 연면적: 10,249.84㎡, 대지면적: 969㎡, 용도: 업무시설",
+        images: [
+            "projects/projectimages/삼성동업무시설1.png",
+            "projects/projectimages/삼성동업무시설2.png",
+            "projects/projectimages/삼성동업무시설3.png",
+            "projects/projectimages/삼성동업무시설4.png",
+            "projects/projectimages/삼성동업무시설5.png"
+        ]
+    },
+
+    "commercial-samsung-competition": {
+        title: "삼성동 업무시설 지명현상 2012",
+        desc: "위치: 서울시 강남구 삼성동, 연면적: 10,633.74㎡, 대지면적: 969㎡, 용도: 업무시설",
+        images: [
+            "projects/projectimages/삼성동지명현상1.png",
+            "projects/projectimages/삼성동지명현상2.png",
+            "projects/projectimages/삼성동지명현상3.png",
+            "projects/projectimages/삼성동지명현상4.png"
+        ]
+    },
+
+    "commercial-dalbuilding": {
+        title: "역삼동 달빌딩 업무시설 2012",
+        desc: "위치: 서울시 강남구 역삼동, 연면적: 3,888.73㎡, 대지면적: 381㎡, 용도: 업무시설",
+        images: [
+            "projects/projectimages/달빌딩1.png",
+            "projects/projectimages/달빌딩2.png",
+            "projects/projectimages/달빌딩3.png",
+            "projects/projectimages/달빌딩4.png",
+            "projects/projectimages/달빌딩5.png"
+        ]
+    },
+
+    "commercial-maepyo": {
+        title: "매표화학 사옥 2011",
+        desc: "위치: 서울시 중구 장충동, 연면적: 1,293.02㎡, 대지면적: 571.90㎡, 규모: 지상 5층 / 지하 1층",
+        images: [
+            "projects/projectimages/매표화학1.png",
+            "projects/projectimages/매표화학2.png",
+            "projects/projectimages/매표화학3.png"
+        ]
+    },
+
+    "commercial-hado": {
+        title: "하노이 HADO HOTEL 2009",
+        desc: "위치: 베트남 하노이, 연면적: 10,991㎡, 대지면적: 1,586㎡, 용도: 호텔",
+        images: [
+            "projects/projectimages/HADO1.png",
+            "projects/projectimages/HADO2.png",
+            "projects/projectimages/HADO3.png",
+            "projects/projectimages/HADO4.png",
+            "projects/projectimages/HADO5.png",
+            "projects/projectimages/HADO6.png",
+            "projects/projectimages/HADO7.png"
+        ]
+    },
+
+    "commercial-mipec": {
+        title: "하노이 MIPEC 주상복합 2009",
+        desc: "위치: 베트남 하노이, 연면적: 188,697㎡, 대지면적: 16,609㎡, 용도: 주상복합",
+        images: [
+            "projects/projectimages/MIPEC1.png"
+        ]
+    },
+
+    "commercial-ansan": {
+        title: "안산 이풀 실내박물관 2013",
+        desc: "위치: 경기도 안산시 팔곡동, 연면적: 1,498.88㎡, 대지면적: 2,995㎡, 용도: 문화 및 박물관 시설",
+        images: [
+            "projects/projectimages/안산이풀1.png",
+            "projects/projectimages/안산이풀2.png",
+            "projects/projectimages/안산이풀3.png"
+        ]
+    },
+
+    "oversea-ssangryeong": {
+        title: "쌍령근린공원 민간공원조성 특례사업",
+        desc: "공원 내 건축물 4개동 with Meier Partners, 경기도 광주시",
+        images: [
+            "projects/projectimages/쌍령근린공원1.png",
+            "projects/projectimages/쌍령근린공원2.png",
+            "projects/projectimages/쌍령근린공원3.png"
+        ]
+    },
+
+    "res-muju": {
+        title: "인천무주골공원 한화포레나 (2021)",
+        images: [
+            "projects/projectimages/인천무주골공원1.png"
+        ],
+        desc: "위치: 인천광역시 연수구 넘말로 81, 연면적: 116,882.835 ㎡, 규모: 지상 23층, 지하 3층, 총 767세대"
+    },
+
+    "res-wonpyeong": {
+        title: "원평동 금오 복합건축물 (2016)",
+        images: [
+            "projects/projectimages/원평동금오1.png",
+            "projects/projectimages/원평동금오2.png",
+            "projects/projectimages/원평동금오3.png"
+        ],
+        desc: "위치: 경상북도 구미시 원평동, 연면적: 6,473.67 ㎡, 대지면적: 608.00 ㎡, 용도: 공동주택 / 업무시설 / 제 2종 근린생활시설"
+    },
+
+    "res-yangbeol": {
+        title: "광주시 양벌리 공동주택 (2015)",
+        images: [
+            "projects/projectimages/광주시양벌리1.png",
+            "projects/projectimages/광주시양벌리2.png"
+        ],
+        desc: "위치: 경기도 광주시 양벌리, 연면적: 464.12 ㎡, 대지면적: 308.60 ㎡, 용도: 공동주택"
+    },
+
+    "res-sangbong": {
+        title: "상봉동 관광숙박 (2015)",
+        images: [
+            "projects/projectimages/상봉동관광숙박1.png",
+            "projects/projectimages/상봉동관광숙박2.png"
+        ],
+        desc: "위치: 서울시 중랑구 상봉동, 연면적: 4,213.00 ㎡, 대지면적: 1,033.70 ㎡, 용도: 관광숙박시설 / 제 2종 근린생활시설"
+    },
+
+    "res-dongcheon": {
+        title: "용인 동천동 단독주택단지 (2015)",
+        images: [
+            "projects/projectimages/용인동천동1.png"
+        ],
+        desc: "위치: 경기도 용인시 수지구 동천동 산9-38번지, 연면적: 5,401.353 ㎡, 규모: 지상 2층 / 지하 1층, 총 35세대"
+    },
+
+    "res-wonju-central": {
+        title: "원주 중앙근린공원 아파트 (2015)",
+        images: [
+            "projects/projectimages/원주중앙근린공원1.png",
+            "projects/projectimages/원주중앙근린공원2.png"
+        ],
+        desc: "위치: 원주시 중앙공원 일원, 대지면적: 77,593.00 ㎡, 규모: 지상 29층 / 지하 1층, 총 1897세대"
+    },
+
+    "res-jikdong": {
+        title: "의정부 직동근린공원 아파트 (2015)",
+        images: [
+            "projects/projectimages/의정부직동근린공원1.png",
+            "projects/projectimages/의정부직동근린공원2.png",
+            "projects/projectimages/의정부직동근린공원3.png"
+        ],
+        desc: "위치: 의정부시 직동공원 일원, 대지면적: 134,996.41 ㎡, 규모: 지상 29층 / 지하 3층, 총 1800세대"
+    },
+
+    "res-okcheon": {
+        title: "옥천 두산위브 아파트 (2015)",
+        images: [
+            "projects/projectimages/옥천두산위브1.png"
+        ],
+        desc: "위치: 충북 옥천군 옥천읍 동안리, 대지면적: 78,985.13 ㎡, 규모: 지상 15층 / 지하 1층, 총 592세대"
+    },
+
+    "res-curiobox": {
+        title: "청라지구 CURIO BOX (2014)",
+        images: [
+            "projects/projectimages/청라지구1.png",
+            "projects/projectimages/청라지구2.png",
+            "projects/projectimages/청라지구3.png",
+            "projects/projectimages/청라지구4.png"
+        ],
+        desc: "위치: 청라지구, 대지면적: 2,378.20 ㎡, 규모: 지상 10층 / 지하 4층, 용도: 업무시설, 제2종근린생활시설"
+    },
+
+    "res-samsong-2014": {
+        title: "고양 삼송지구 (2014)",
+        images: [
+            "projects/projectimages/삼송지구1.png",
+            "projects/projectimages/삼송지구2.png",
+            "projects/projectimages/삼송지구3.png",
+            "projects/projectimages/삼송지구4.png",
+            "projects/projectimages/삼송지구5.png"
+        ],
+        desc: "위치: 고양 삼송지구 2014, 대지면적: 3,238.00 ㎡, 연면적: 57,777.00 ㎡, 용도: 업무시설(오피스텔), 판매시설"
+    },
+
+    "res-hannam": {
+        title: "용산구 한남동 OO빌딩 리모델링 (2013)",
+        images: [
+            "projects/projectimages/용산구한남동1.png",
+            "projects/projectimages/용산구한남동2.png",
+            "projects/projectimages/용산구한남동3.png"
+        ],
+        desc: "위치: 서울시 용산구 한남동 682-5번지, 연면적: 709.63 ㎡, 규모: 지상 4층 / 지하 1층"
+    },
+
+    "res-sindang": {
+        title: "신당동 주상복합 (2012)",
+        images: [
+            "projects/projectimages/신당동주상복합1.png",
+            "projects/projectimages/신당동주상복합2.png",
+            "projects/projectimages/신당동주상복합3.png"
+        ],
+        desc: "위치: 서울특별시 중구 신당동, 대지면적: 711.2 ㎡, 규모: 지상 17층 / 지하 4층, 총 173세대"
+    },
+
+    "res-galhyeon": {
+        title: "은평구 갈현동 OO다세대주택 (2012)",
+        images: [
+            "projects/projectimages/은평구갈현동1.png"
+        ],
+        desc: "위치: 서울시 은평구 갈현동 327-83번지, 대지면적: 686.73 ㎡, 규모: 지상 4층 / 지하 1층"
+    },
+
+    "res-bangbae": {
+        title: "서초구 방배동 OO다세대주택 (2012)",
+        images: [
+            "projects/projectimages/서초구방배동1.png"
+        ],
+        desc: "위치: 서울시 서초구 방배동 327-3, 대지면적: 321.28 ㎡, 규모: 지상 5층"
+    },
+
+    "res-gogi": {
+        title: "용인시 수지구 고기동 단독주택 (2012)",
+        images: [
+            "projects/projectimages/용인시수지구1.png",
+            "projects/projectimages/용인시수지구2.png"
+        ],
+        desc: "위치: 경기도 용인시 수지구 고기동, 규모: 지상 2층 / 지하 1층"
+    },
+
+    "res-myeonmok": {
+        title: "면목동 도시형 생활주택 (2011)",
+        images: [
+            "projects/projectimages/면목동도시형1.png",
+            "projects/projectimages/면목동도시형2.png",
+            "projects/projectimages/면목동도시형3.png"
+        ],
+        desc: "위치: 서울특별시 중랑구 면목동, 대지면적: 3,040.9 ㎡, 규모: 지상 10층 / 지하 1층, 총 112세대"
+    },
+
+    "res-cheongsu": {
+        title: "천안시 청수동 경남 아너스빌 (2010)",
+        images: [
+            "projects/projectimages/천안시청수동1.png",
+            "projects/projectimages/천안시청수동2.png"
+        ],
+        desc: "위치: 충청남도 천안시 청수동 일원 , 대지면적: 3,792.43 ㎡, 총 230세대"
+    },
+
+    "res-songpa": {
+        title: "서울시 송파동 OO 공동주택 (2010)",
+        images: [
+            "projects/projectimages/서울시송파동1.png",
+            "projects/projectimages/서울시송파동2.png"
+        ],
+        desc: "위치: 서울시 송파구 송파동 99-2번지, 대지면적: 34,013.69 ㎡, 규모: 지상20층, 지하1층, 총 514세대"
+    },
+
+    "res-gasan": {
+        title: "가산 도시형 생활주택 (2010)",
+        images: [
+            "projects/projectimages/가산도시형1.png"
+        ],
+        desc: "위치: 서울특별시 금천구 가산동, 대지면적: 663 ㎡, 총 141세대, 용도: 공장 / 편의시설"
+    },
+
+    "res-gildong": {
+        title: "길동 도시형 생활주택 A,B블럭 (2010)",
+        images: [
+            "projects/projectimages/길동도시형1.png",
+            "projects/projectimages/길동도시형2.png",
+            "projects/projectimages/길동도시형3.png",
+            "projects/projectimages/길동도시형4.png"
+        ],
+        desc: "위치: 서울특별시 강동구 길동, 대지면적: 1,455 ㎡, 총 128세대, 규모: 지상15층, 지하1층"
+    },
+
+    "res-manseok": {
+        title: "인천 만석지구 (2010)",
+        images: [
+            "projects/projectimages/인천만석지구1.png"
+        ],
+        desc: "위치: 인천 만석지구, 대지면적: 107,436.258 ㎡, 총 1273세대"
+    },
+
+    "res-jangchung": {
+        title: "장충동 도시형 생활주택 (2009)",
+        images: [
+            "projects/projectimages/장충동도시형1.png",
+            "projects/projectimages/장충동도시형2.png",
+            "projects/projectimages/장충동도시형3.png"
+        ],
+        desc: "위치: 서울특별시 중구 장충동, 대지면적: 892 ㎡, 규모: 지상 10층 / 지하 1층, 총 96세대"
+    },
+
+    "res-wacheon": {
+        title: "천안시 와천동 주상복합 아파트",
+        images: [
+            "projects/projectimages/와천동주상복합1.png"
+        ],
+        desc: "위치: 충청남도 천안시 와천동, 대지면적: 203,865.67 ㎡, 규 : 지상 48층, 지하 5층"
+    },
+
+    "res-bongmyeong": {
+        title: "대전 봉명동 주상복합 아파트",
+        images: [
+            "projects/projectimages/봉명동주상복합1.png"
+        ],
+        desc: "위치: 대전광역시 유성구 봉명동 541-9, 대지면적: 118,502.00 ㎡, 규 : 지상 49, 지하 5층"
+    },
+
+    "res-samsong-rental": {
+        title: "고양 삼송 비축용 임대주택 (2008)",
+        images: [
+            "projects/projectimages/고양삼송비축용1.png",
+            "projects/projectimages/고양삼송비축용2.png",
+            "projects/projectimages/고양삼송비축용3.png"
+        ],
+        desc: "위치: With DA, 위치: 고양시 삼송택지개발지구, 대지면적: 63,188.450 ㎡, 규모: 지상 25층 / 지하 2층, 총 1045세대, 발주처: 한국토지공사"
+    },
+
+    "res-hansol": {
+        title: "분당 정자동 한솔마을 주공5단지 리모델링 (2008)",
+        images: [
+            "projects/projectimages/한솔마을1.png",
+            "projects/projectimages/한솔마을2.png",
+            "projects/projectimages/한솔마을3.png",
+            "projects/projectimages/한솔마을4.png"
+        ],
+        desc: "위치: 경기도 성남시 분당구 정자동, 대지면적: 45,046 ㎡, 규모: 지상 26층 / 지하 2층, 총 1156세대, 시공사: 동부건설, 현대산업개발 컨소시엄"
+    },
+
+    "res-hyundai1": {
+        title: "대치동 현대1차 아파트 리모델링 (2008)",
+        images: [
+            "projects/projectimages/현대1차1.png",
+            "projects/projectimages/현대1차2.png",
+            "projects/projectimages/현대1차3.png",
+            "projects/projectimages/현대1차4.png"
+        ],
+        desc: "위치: 서울시 강남구 대치동, 대지면적: 4,848 ㎡, 규모: 지상 16층 / 지하 3층, 총 120세대, 시공사: 현대산업개발"
+    },
+
+    "res-mokdong-yuwon": {
+        title: "목동 유원아파트 리모델링 (2008)",
+        images: [
+            "projects/projectimages/유원아파트1.png",
+            "projects/projectimages/유원아파트2.png",
+            "projects/projectimages/유원아파트3.png",
+            "projects/projectimages/유원아파트4.png"
+        ],
+        desc: "위치: 서울시 양천구 신정동, 대지면적: 6,198 ㎡, 규모: 지상 16층 / 지하 3층, 총 156세대, 시공사: 현대산업개발"
+    },
+
+    "res-dunchon": {
+        title: "둔촌동 현대1차 리모델링 (2007)",
+        images: [
+            "projects/projectimages/둔촌동현대1.png",
+            "projects/projectimages/둔촌동현대2.png",
+            "projects/projectimages/둔촌동현대3.png",
+            "projects/projectimages/둔촌동현대4.png"
+        ],
+        desc: "위치: 서울시 강동구 둔촌동, 대지면적: 27,411 ㎡, 규모: 지상 14층 / 지하 1층, 총 498세대, 시공사: 현대산업개발"
+    },
+
+    "res-ansan": {
+        title: "안산 OO주상복합 계획안 (2007)",
+        images: [
+            "projects/projectimages/안산주상복합1.png",
+            "projects/projectimages/안산주상복합2.png",
+            "projects/projectimages/안산주상복합3.png",
+            "projects/projectimages/안산주상복합4.png"
+        ],
+        desc: "위치: 경기도 안산시, 연면적: 653,254 ㎡, 대지면적 : 63,935 ㎡, 규모: 지상 30층 / 지하 2층, 총 2296세대"
+    },
+
+    "res-udong": {
+        title: "부산 해운대 우동 주상복합 (2006)",
+        images: [
+            "projects/projectimages/우동주상복합1.png",
+            "projects/projectimages/우동주상복합2.png",
+            "projects/projectimages/우동주상복합3.png",
+            "projects/projectimages/우동주상복합4.png",
+            "projects/projectimages/우동주상복합5.png",
+            "projects/projectimages/우동주상복합6.png"
+        ],
+        desc: "위치: With DA, 위치: 부산광역시 해운대구 우동, 연면적: 95,714 ㎡, 대지면적: 8,109 ㎡, 총 292세대, 도시철도공사 현상설계 최우수작 당선"
+    },
+
+    "res-masan": {
+        title: "마산 회원 3구역 재개발 (2006)",
+        images: [
+            "projects/projectimages/마산회원1.png",
+            "projects/projectimages/마산회원2.png"
+        ],
+        desc: "위치: 경상남도 마산시 회원동, 연면적: 173,555 ㎡, 대지면적 : 59,247 ㎡, 규모: 지상 30층 / 지하 2층, 총 1174세대"
+    },
+
+    "res-pyeongchang": {
+        title: "평창동 롯데캐슬 포디움 (2006)",
+        images: [
+            "projects/projectimages/롯데캐슬1.png",
+            "projects/projectimages/롯데캐슬2.png",
+            "projects/projectimages/롯데캐슬3.png",
+            "projects/projectimages/롯데캐슬4.png"
+        ],
+        desc: "위치: With DA, 위치: 서울시 종로구 평창동, 대지면적 : 17,236.70 ㎡, 규모: 지상 12층 / 지하 1층, 총 113세대"
+    },
+
+    "res-samsung": {
+        title: "삼성동 단독주택 (2014)",
+        images: [
+            "projects/projectimages/삼성동단독주택1.png",
+            "projects/projectimages/삼성동단독주택2.png",
+            "projects/projectimages/삼성동단독주택3.png"
+        ],
+        desc: "위치: 서울특별시 강남구 삼성동, 대지면적: 496.10 ㎡, 규모: 지상 2층 / 지하 2층, 용도 : 단독주택"
+    },
+
+    "res-tuscany": {
+        title: "투스카니힐스 Exterior (2011)",
+        images: [
+            "projects/projectimages/투스카니힐스1.png",
+            "projects/projectimages/투스카니힐스2.png"
+        ],
+        desc: "위치: 경기도 용인시 이동면 서리, 코리아CC투스카니힐스"
+    },
+    // Prize-Winning Projects
+    "prize-sejong": {
+        title: "세종대로 역사문화공간 (2015)",
+        desc: "가작 2015, with 기술사 사무소 이수",
+        images: [
+            "projects/projectimages/역사문화1.png",
+            "projects/projectimages/역사문화2.png",
+            "projects/projectimages/역사문화3.png",
+            "projects/projectimages/역사문화4.png",
+            "projects/projectimages/역사문화5.png",
+            "projects/projectimages/역사문화6.png"
+        ]
+    },
+
+    "prize-jamsugyo": {
+        title: "잠수교, 문화의 다리 (2024)",
+        desc: "국제현상설계공모 2024, with Arch Mist, 서울특별시 잠수교",
+        images: [
+            "projects/projectimages/잠수교1.png",
+            "projects/projectimages/잠수교2.png"
+        ]
     }
 
 };
